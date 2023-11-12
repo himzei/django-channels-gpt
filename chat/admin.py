2d7a11f7802c153f472ba5ee7bf0bcdd9b6a6a74
조현일 <himzei@gmail.com>
2023-11-12 17:57:08 +0900

    init

---
+++ chat/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from .models import RolePlayingRoom
+from .forms import RolePlayingRoomForm
+
+@admin.register(RolePlayingRoom)
+class RolePlayingRoomAdmin(admin.ModelAdmin):
+    form = RolePlayingRoomForm
+
+    def save_model(self, request, obj, form, change):
+        if change is False and form.is_valid():
+            obj.user = request.user
+
+        super().save_model(request, obj, form, change)
